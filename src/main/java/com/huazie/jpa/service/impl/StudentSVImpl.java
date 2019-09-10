d72b60db48cdc8159126643cdce42cba9abcc8c3
huazie <huazie.lgh@gmail.com>
2019-09-10 08:13:30 +0800

    huazie_flea-jpa-test 服务层实现_MOD

--- src/main/java/com/huazie/jpa/service/impl/StudentSVImpl.java
+++ src/main/java/com/huazie/jpa/service/impl/StudentSVImpl.java
@@ -1,5 +1,6 @@
 package com.huazie.jpa.service.impl;
 
+import com.huazie.frame.db.common.exception.DaoException;
 import com.huazie.jpa.dao.interfaces.IStudentDAO;
 import com.huazie.jpa.entity.Student;
 import com.huazie.jpa.service.interfaces.IStudentSV;
@@ -8,6 +9,8 @@ import com.huazie.frame.db.jpa.service.impl.AbstractFleaJPASVImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p> 学生SV层实现类 </p>
@@ -22,6 +25,16 @@ public class StudentSVImpl extends AbstractFleaJPASVImpl<Student> implements ISt
     @Autowired
     @Qualifier("studentDAO")
     private IStudentDAO studentDao;
+
+    @Override
+    public List<Student> getStudentList(String name, Integer sex, Integer minAge, Integer maxAge, int pageNum, int pageCount) throws DaoException {
+        return studentDao.getStudentList(name, sex, minAge, maxAge, pageNum, pageCount);
+    }
+
+    @Override
+    public long getStudentCount(String name, Integer sex, Integer minAge, Integer maxAge) throws DaoException {
+        return studentDao.getStudentCount(name, sex, minAge, maxAge);
+    }
 
     @Override
     protected IAbstractFleaJPADAO<Student> getDAO() {
